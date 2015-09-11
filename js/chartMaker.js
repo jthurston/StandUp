5f50c05c7d00c980f3d97a625e027beee1702487
Jeff Thurston <jeffery.m.thurston@saic.com>
2015-09-11 08:33:17 -0400

    Initial commit

---
+++ js/chartMaker.js
@@ -0,0 +1,30 @@
+function testing(){
+
+console.log("In Chart Maker" + team)
+
+var chart = new Highcharts.Chart({
+    chart: {
+        renderTo: 'container',
+        type: 'pie'
+    },
+    title: {
+        text: 'Participant Times'
+    },
+
+    series: [{
+        name: "Times",
+        colorByPoint: true,
+        data: team
+    }]
+});
+
+}
+// the button action
+//$('#niceButton').click(function() {
+//  console.log("Team" + team[0].name + " Time " + team[0].y)
+//    chart.series[0].setData([
+//      {
+//      data: team
+//    }
+//  ]);
+//});
---
+++ js/timer.js
@@ -0,0 +1,96 @@
+var prevTime, stopwatchInterval, elapsedTime = 0;
+
+var updateTime = function () {
+  var tempTime = elapsedTime;
+  var milliseconds = tempTime % 1000;
+  tempTime = Math.floor(tempTime / 1000);
+  var seconds = tempTime % 60;
+  tempTime = Math.floor(tempTime / 60);
+  var minutes = tempTime % 60;
+  tempTime = Math.floor(tempTime / 60);
+  var hours = tempTime % 60;
+
+  //Make a string of time to display
+  var time = hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+  //var time = hours + ":" + minutes + ":" + seconds
+  userTime = seconds % 60;
+
+  if (seconds >= warnTime){
+    $('#div' + whichOne).css('background-color', 'yellow');
+  }
+
+  if (seconds >= maxTime){
+    $('#div' + whichOne).css('background-color', 'red');
+  }
+
+  $("#time").text(time);
+  $('#time' + currentSpeaker).text(time);
+
+  //Change time on currentspeaker for graphing
+  team[currentSpeaker]['y'] = userTime;
+};
+
+//$("#startButton").click(function () {
+$('[id^="start"]').click(function () {
+
+  if (!stopwatchInterval) {
+    stopwatchInterval = setInterval(function () {
+      if (!prevTime) {
+        prevTime = Date.now();
+      }
+      elapsedTime += Date.now() - prevTime;
+      prevTime = Date.now();
+      updateTime();
+    }, 50);
+  }
+});
+
+$("#pauseButton").click(function () {
+
+  if (stopwatchInterval) {
+    clearInterval(stopwatchInterval);
+    stopwatchInterval = null;
+  }
+  prevTime = null;
+});
+
+$("#resetButton").click(function () {
+
+  $("#pauseButton").click();
+  elapsedTime = 0;
+  $('#div' + whichOne).css('background-color', onColor);
+  updateTime();
+});
+
+function startTimer(){
+  if (!stopwatchInterval) {
+    stopwatchInterval = setInterval(function () {
+      if (!prevTime) {
+        prevTime = Date.now();
+      }
+      elapsedTime += Date.now() - prevTime;
+      prevTime = Date.now();
+      updateTime();
+    }, 50);
+  }
+}
+
+function pauseTimer(){
+  if (stopwatchInterval) {
+    clearInterval(stopwatchInterval);
+    stopwatchInterval = null;
+  }
+  prevTime = null;
+}
+
+function resetTimer(){
+  pauseTimer();
+  elapsedTime = 0;
+  $('#div' + whichOne).css('background-color', onColor);
+  updateTime();
+}
+
+$(document).ready(function () {
+  updateTime();
+
+});
