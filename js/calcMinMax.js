3b6aa9e3770a995fe60b17ca7b301e4de8bacc0b
Jeffery Thurston <jeffery.m.thurston@saic.com>
2015-10-23 17:02:18 -0400

    Added the skip functionality, fixed the volunteer functionality, created visual display for what type of presenter they were (skipped, volunteered random)

--- js/calcMinMax.js
+++ js/calcMinMax.js
@@ -3,6 +3,7 @@ $.getJSON(jsonFile, function(data){
   maxTime = totalTime/data.length;
   warnTime = (totalTime/data.length) - 1;
 
+  $('#maxTimed').text(maxTime + " Minutes");
   //alert(maxTime);
   //alert(warnTime);
 })
