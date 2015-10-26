80e70944c767882a581b5039769d8ab905cfb572
Jeff Thurston <jeff.thurston@gmail.com>
2015-10-26 06:51:40 -0400

    fixed calculation difference for max time and added skeleton.css for future layout fixes

--- js/calcMinMax.js
+++ js/calcMinMax.js
@@ -1,6 +1,6 @@
 $.getJSON(jsonFile, function(data){
 
-  maxTime = totalTime/data.length;
+  maxTime = (totalTime/data.length).toFixed(2); 
   warnTime = (totalTime/data.length) - 1;
 
   $('#maxTimed').text(maxTime + " Minutes");
